fd6303cc6f68cf0ffdc982bc1952cd812c7950a8
Alexander Mykhailenko <mixajlenko.alexander@gmail.com>
2021-05-18 01:50:51 +0300

    path to jsp and empty servlet impl

---
+++ src/main/java/com/mixajlenko/epam/finaltask/ispsystem/controller/command/path/Path.java
@@ -0,0 +1,18 @@
+package com.mixajlenko.epam.finaltask.ispsystem.controller.command.path;
+
+public final class Path {
+
+    // pages
+    public static final String PAGE__LOGIN = "/login.jsp";
+    public static final String PAGE__ERROR_PAGE = "/WEB-INF/jsp/errorPage.jsp";
+    public static final String PAGE__REGISTRATION_PAGE = "/WEB-INF/jsp/registration.jsp";
+    public static final String PAGE__MAIN_PAGE = "/WEB-INF/jsp/client/mainPageUser.jsp";
+    public static final String PAGE__SERVICE_PAGE = "/WEB-INF/jsp/client/servicePage.jsp";
+    public static final String PAGE__PAYMENT_SYSTEM_PAGE = "/WEB-INF/jsp/client/paymentSystemPage.jsp";
+    public static final String PAGE__SUPPORT_PAGE = "/WEB-INF/jsp/client/supportPage.jsp";
+    public static final String PAGE__MAIN_PAGE_ADMIN = "/WEB-INF/jsp/admin/mainPageAdmin.jsp";
+    public static final String PAGE__CHANGE_PASSWORD = "/WEB-INF/jsp/client/changePassPage.jsp";
+    public static final String PAGE__MANAGE_PLAN_ADMIN = "/WEB-INF/jsp/admin/managePlan.jsp";
+    public static final String PAGE__MANAGE_USERS_ADMIN = "/WEB-INF/jsp/admin/manageUsers.jsp";
+
+}
