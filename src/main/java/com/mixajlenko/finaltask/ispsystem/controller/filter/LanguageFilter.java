2d9be4b8c47cd34dfae0e51f75026c141e8b440f
Alexander Mykhailenko <mixajlenko.alexander@gmail.com>
2021-06-04 03:38:26 +0300

    localization filter impl

---
+++ src/main/java/com/mixajlenko/finaltask/ispsystem/controller/filter/LanguageFilter.java
@@ -0,0 +1,55 @@
+package com.mixajlenko.finaltask.ispsystem.controller.filter;
+
+import jakarta.servlet.*;
+import org.apache.log4j.Logger;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+
+public class LanguageFilter implements Filter {
+
+    private static Logger logger = Logger.getLogger(LanguageFilter.class);
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+
+    }
+
+    @Override
+    public void doFilter(ServletRequest servletRequest,
+                         ServletResponse servletResponse,
+                         FilterChain filterChain) throws IOException, ServletException {
+        logger.info("TEST");
+        final HttpServletRequest req = (HttpServletRequest) servletRequest;
+        final HttpServletResponse resp = (HttpServletResponse) servletResponse;
+
+        String path = req.getRequestURI();
+        path = path.replaceAll("language/", "");
+        if (path.equals("/view//")) {
+            path = "/";
+        }
+
+        logger.info("uri" + req.getRequestURI());
+        logger.info("path" + path);
+
+        String language = req.getParameter("language");
+        boolean isEnglish = language.equals("EN");
+        boolean isRussian = language.equals("RU");
+
+        if (isEnglish) {
+            req.getSession().setAttribute("language", "en-EN");
+        } else if (isRussian) {
+            req.getSession().setAttribute("language", "ru-RU");
+        }
+
+        req.getRequestDispatcher(path).forward(req, resp);
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+}
