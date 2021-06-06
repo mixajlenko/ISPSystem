8a44aaa01ec6df1b615b0ea7e21b4556f33f3615
Alexander Mykhailenko <mixajlenko.alexander@gmail.com>
2021-06-06 17:47:37 +0300

    fixes connectionPool for heroku

--- src/main/java/com/mixajlenko/finaltask/ispsystem/dao/connection/ConnectionFactory.java
+++ src/main/java/com/mixajlenko/finaltask/ispsystem/dao/connection/ConnectionFactory.java
@@ -5,13 +5,8 @@ import javax.naming.InitialContext;
 import javax.naming.NamingException;
 import javax.sql.DataSource;
 
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.InputStream;
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.SQLException;
-import java.util.Properties;
 
 public class ConnectionFactory {
 
